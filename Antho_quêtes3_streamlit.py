310d29cd46e40277558cb444756225f1cd3c6cce
Anthony <anthonygoulet37@gmail.com>
2025-12-04 10:43:25 +0100

    Initialisation propre du projet

---
+++ Antho_quêtes3_streamlit.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pandas as pd
+from streamlit_authenticator import Authenticate
+from streamlit_option_menu import option_menu
+
+
+import streamlit as st
+
+lesDonneesDesComptes = {
+    'usernames': {
+        'utilisateur': {
+            'name': 'utilisateur',
+            'password': 'utilisateurMDP',
+            'email': 'utilisateur@gmail.com',
+            'failed_login_attemps': 0,
+            'logged_in': False,
+            'role': 'utilisateur'
+        },
+        'Bob': {
+            'name': 'Bob',
+            'password': 'éponge',
+            'email': 'admin@gmail.com',
+            'failed_login_attemps': 0,
+            'logged_in': False,
+            'role': 'administrateur'
+        }
+    }
+}
+
+authenticator = Authenticate(
+    lesDonneesDesComptes,
+    "cookie name",
+    "cookie key",
+    30,
+)
+
+ # --- Login box ---
+authenticator.login()
+
+
+
+
+# Création du menu qui va afficher les choix qui se trouvent dans la variable options
+
+with st.sidebar:
+    
+    st.write("Bienvenue root")
+    selection = option_menu(
+        menu_title=None,
+        options = ["Accueil", "🐱Les photos de mon chat"]
+    
+    )
+
+
+if selection == "Accueil":
+    st.write("Bienvenue sur ma page Streamlit !")
+    st.image("Bienvenue.jpg")
+
+elif selection == "🐱Les photos de mon chat":
+    st.write("Bienvenue dans l'album de mon chat")
+        # Création de 3 colonnes 
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image("Cat1.jpg")
+
+    with col2:
+        st.image("Cat2.jpg")
+
+    with col3:
+        st.image("Cat3.jpg")
+
+
+if st.session_state["authentication_status"]:
+   # Le bouton de déconnexion
+  authenticator.logout("Déconnexion")
+
+elif st.session_state["authentication_status"] is False:
+    st.error("L'username ou le password est/sont incorrect")
+elif st.session_state["authentication_status"] is None:
+    st.warning('Les champs username et mot de passe doivent être remplie')
+
